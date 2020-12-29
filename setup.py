cb40f696e0d92f43ac16cdcbcc64acc1d93a615f
Sudo-Pluralize-Abenson <65480364+Sudo-Pluralize-Abenson@users.noreply.github.com>
2020-12-28 19:27:02 -0800

    Tweaks

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='artpy',
+      version='0.1',
+      description='A suite of art/visually pleasing image generation tools, this module will hopefully grow with time',
+      url='',
+      author='Alexander Benson',
+      author_email='alexander.n.benson@gmail.com',
+      license='BSD-3',
+      packages=['hello'],
+      zip_safe = False)
