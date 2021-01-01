96770167789de0199ba9403326a8fc3647f69aed
Sudo-Pluralize-Abenson <65480364+Sudo-Pluralize-Abenson@users.noreply.github.com>
2020-12-31 16:09:02 -0800

    preparing the drawing tools

--- pathObj.py
+++ pathObj.py
@@ -6,7 +6,12 @@ class PathObject():
     """Create single path from a line-like image"""
     def __init__(self, imageArray, threshold = 50):
         self.imageArray = imageArray
-        self.imageArray[self.imageArray > threshold] = 1
+        for i in range(len(self.imageArray)):
+            for j in range(len(self.imageArray[0])):
+                if self.imageArray[i][j] > threshold:
+                    self.imageArray[i][j] = 1
+                else:
+                    self.imageArray[i][j] = 0
 
     def show(self):
         plt.imshow(self.imageArray)
